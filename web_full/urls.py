cdcb80d8e9b38c8fe7ccd5e7e2366e8228a2771f
DavidEscamilla <davidgarescamilla@gmail.com>
2019-05-12 18:32:17 +0200

    subidas todas las pantallas corregido

--- web_full/urls.py
+++ web_full/urls.py
@@ -5,6 +5,8 @@ from . import Ajax
 urlpatterns = [
      path('', views.index_view, name='index_view'),
      path('about/', views.about, name='about'),
+     path('tutorial/', views.tutorial, name='tutorial'),
+     path('contact/', views.contact, name='contact'),
      path('test/', Ajax.test, name='test'),
      path('datos/', Ajax.Paso_de_datos, name='Paso_de_datos'),
 ]
