4251b62fbc86d954e1fcee49dfe5d92df68c1a40
DavidEscamilla <davidgarescamilla@gmail.com>
2019-05-12 18:30:23 +0200

    subidas todas las pantallas

--- web_full/views.py
+++ web_full/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 
 def index_view(request):
     return render(request, 'web/index.html', {})
+
+
+def about(request):
+    return render(request, 'web/about.html', {})
+
+
+def tutorial(request):
+    return render(request, 'web/tutorial.html', {})
+
+
+def contact(request):
+    return render(request, 'web/contact.html', {})
