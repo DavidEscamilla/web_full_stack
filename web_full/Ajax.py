89b85843c13b69a73949c0a58807ecc973d800e6
DavidEscamilla <davidgarescamilla@gmail.com>
2019-05-13 00:33:03 +0200

    send_email

--- web_full/Ajax.py
+++ web_full/Ajax.py
@@ -1,5 +1,6 @@
 # coding=utf-8
 from django.http import HttpResponse
+from . import send_email
 import json
 
 
@@ -11,8 +12,11 @@ def test(request):
 def Paso_de_datos(request):
     d_respuesta = {}
     form = request.POST
-    email = form.get('email', None)
-    print (email)
+    subject = form.get('subject', "anonymous")
+    in_message = form.get('in_message', None)
+    message = 'Subject: {}\n\n{}'.format(subject, in_message)
+    print (message)
+    send_email.Send_Email().send(message)
 
     if request:
         d_respuesta['result'] = ('Email registrado')
---
+++ web_full/send_email.py
@@ -0,0 +1,16 @@
+import smtplib
+import ssl
+
+
+class Send_Email():
+    """docstring for ."""
+
+    def send(self, message):
+        port = 465  # For SSL
+        smtp_server = "smtp.gmail.com"
+        receiver_email = "davidgarescamilla@gmail.com"
+        password = "Ramonflores1945d12"  # Enter receiver address
+        context = ssl.create_default_context()
+        with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+            server.login(receiver_email, password)
+            server.sendmail(receiver_email, receiver_email, message)
